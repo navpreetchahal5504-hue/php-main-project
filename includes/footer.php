1c1a3dc005782f8c3807120e177f9aff4ec90e34
navpreetchahal5504-hue <navpreetchahal5504@gmail.com>
2026-08-06 20:51:26 -0400

    Add footer section to footer.php

---
+++ includes/footer.php
@@ -0,0 +1,48 @@
+</main>
+
+
+
+<footer class="bg-dark text-white text-center mt-5 p-4">
+
+
+<div class="container">
+
+
+<p class="mb-1">
+
+Inventry Management System
+
+</p>
+
+
+<p class="mb-0">
+
+COMP1006- Intro to Web Programming Using PHP
+
+</p>
+
+
+<p class="mb-0">
+
+© 2026 All Rights are reserved
+
+</p>
+
+
+</div>
+
+
+</footer>
+
+
+<!--- Bootstrip Javascript --->
+
+<script 
+src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js">
+</script>
+
+
+
+</body>
+
+</html>
