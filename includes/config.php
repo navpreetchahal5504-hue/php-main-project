ccab75b95900b698cd37c2e09e288b7e09a1dd11
navpreetchahal5504-hue <navpreetchahal5504@gmail.com>
2026-08-06 23:18:56 -0400

    Update database configuration for new server

--- includes/config.php
+++ includes/config.php
@@ -1,13 +1,12 @@
 <?php
 
-define("DB_HOST","localhost");
+define("DB_HOST", "sql312.infinityfree.com");
 
-define("DB_NAME","inventory_system");
+define("DB_NAME", "if0_42597226_XXX");
 
-define("DB_USER","root");
+define("DB_USER", "if0_42597226");
 
-define("DB_PASS","");
-
+define("DB_PASS", "vcEt3QXJ4V");
 
 session_start();
 
